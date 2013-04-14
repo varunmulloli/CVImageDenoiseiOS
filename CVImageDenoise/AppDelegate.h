99a215c972d5fa6e9f4dc105a7fe297b45c9fa66
mu1101i <mulloli@me.com>
2013-04-14 10:50:45 +0530

    Initial Commit

---
+++ CVImageDenoise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CVImageDenoise
+//
+//  Created by Varun Mulloli on 14/04/13.
+//  Copyright (c) 2013 Varun Mulloli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
